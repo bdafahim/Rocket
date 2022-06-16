b22bd0c105b3f2d84d994881dfe2c36782d345ee
bdafahim <badrudduza20@student.sust.edu>
2022-06-16 12:41:26 +0600

    mat cleanup

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,36 +1,16 @@
 import {Component, OnInit, TemplateRef} from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
 import { DashboardChartsData, IChartProps } from './dashboard-charts-data';
-import {MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {ModelDetailsDialogComponent} from "./dialogs/model-details-dialog/model-details-dialog.component";
-import {MachineLearningModel} from "./types/machine-learning-model";
 import {DashboardService} from "../../service/dashboard/dashboard.service";
 import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
 
-interface IUser {
-  name: string;
-  state: string;
-  registered: string;
-  country: string;
-  usage: number;
-  period: string;
-  payment: string;
-  activity: string;
-  avatar: string;
-  status: string;
-  color: string;
-}
 
 @Component({
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  detailsModalRef: MatDialogRef<ModelDetailsDialogComponent, any> | undefined;
   constructor(private chartsData: DashboardChartsData,
               private modalService: BsModalService,
-              private dialog: MatDialog,
               private dashboardService: DashboardService
               ) {
   }
@@ -57,21 +37,5 @@ export class DashboardComponent implements OnInit {
         this.modalRef = this.modalService.show(template);
       }
     )
-    // this.detailsModalRef = this.dialog.open(ModelDetailsDialogComponent,{
-    //   data,
-    //   width: '600px',
-    //   maxWidth: '600px',
-    //   height: 'auto',
-    //   panelClass: 'dialog-confirmation'
-    // })
-  }
-  openModal(template: TemplateRef<any>, data: any) {
-    this.detailsModalRef = this.dialog.open(ModelDetailsDialogComponent,{
-      data,
-      width: '600px',
-      maxWidth: '600px',
-      height: 'auto',
-      panelClass: 'dialog-confirmation'
-    })
   }
 }
